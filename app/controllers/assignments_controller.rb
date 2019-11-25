160bea7da99d3d4a0ad182f8ae45b8830d721efe
garimas <garimasarangal@gmail.com>
2019-11-25 17:04:39 -0500

    Revert "adding link to join" Reverting This reverts commit 6a072025db11f9a1e81d974d384f1154817c1a75.

--- app/controllers/assignments_controller.rb
+++ app/controllers/assignments_controller.rb
@@ -383,7 +383,7 @@ class AssignmentsController < ApplicationController
 
  #This method will be called when we have assignment which is a conference and link to join the assignment
   def conference_assignment_joining_link
-   "https://expertiza.ncsu.edu/user"
+    # call new controller here
   end
 
   def assignment_form_params
